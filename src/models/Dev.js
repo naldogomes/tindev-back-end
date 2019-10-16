e30abae41f9debc2a5a889a6634fc7dd023b58fc
Redinaldo Gomes <redi.gomes@gmail.com>
2019-10-16 00:08:10 -0300

    Setting controllers config

---
+++ src/models/Dev.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const DevSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: String,
+        required: true,
+    },
+    bio: String,
+    avatar: {
+        type: String,
+        required: true,
+    },
+    likes: [{
+        type: Schema.Types.ObjectID,
+        ref: 'Dev',
+    }],
+    deslikes: [{
+        type: Schema.Types.ObjectID,
+        ref: 'Dev',
+    }],
+}, {
+    timestamps: true,
+})
+
+module.exports = model('Dev', DevSchema)
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
 const express = require('express')
+const DevController = require('./controllers/DevController')
+const LikeController = require('./controllers/LikeController') 
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
-})
-
-routes.post('/devs', (req, res) => {
-    console.log(req.body.name)
-
-    return res.json({ ok: true });
-})
+routes.post('/devs', DevController.store); 
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
